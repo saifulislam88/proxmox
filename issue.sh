684385e6d167f9aa53d7eadc0fa69846349b2dd6
Saiful Islam <68442870+saifulislam88@users.noreply.github.com>
2025-01-23 11:37:44 +0600

    Update issue.sh

--- issue.sh
+++ issue.sh
@@ -1,1 +1,9 @@
 
+```sh
+systemctl status supervisor
+systemctl status filebeat
+systemctl status nginx
+systemctl status redis or systemctl status redis-server
+systemctl status memcached
+systemctl status node_exporter.service
+```
